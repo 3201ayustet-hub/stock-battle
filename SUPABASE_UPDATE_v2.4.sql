f5c23c29ed56b17e8296d11b3c009c9751221076
3201ayustet-hub <3201ayustet@gmail.com>
2026-08-10 16:34:55 +0900

    Delete SUPABASE_UPDATE_v2.4.sql

--- SUPABASE_UPDATE_v2.4.sql
+++
@@ -1,5 +0,0 @@
--- Stock Battle Ver.2.4
--- Ver.2.4ではSupabaseのデータベース構造・RPCに変更はありません。
--- Ver.2.2以降の完全版SQLを実行済みなら、このSQL操作は不要です。
-
-select 'Stock Battle Ver.2.4: database update is not required.' as result;
